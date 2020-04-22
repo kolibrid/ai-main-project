4024177eba643a0c03fb9e40fa21bf71feb49044
Álvaro Martínez Fernández <alvaro@ramsalt.com>
2020-04-22 20:22:50 +0200

    Fixed songs error

--- pandas/main.py
+++ pandas/main.py
@@ -25,38 +25,41 @@ print(song_df.head())
 
 '''
 
+# This step is needed and is not in the example
+song_df['song'] = song_df["artist_name"] + ' - ' + song_df["title"]
+
 # We select a subset of this data (the first 10,000 songs). We then merge the song and artist_name
 # into one column, aggregated by number of time a particular song is listened too in general by all users.
-song_grouped = song_df.groupby(['title']).agg({'listen_count': 'count'}).reset_index()
+song_grouped = song_df.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
 grouped_sum = song_grouped['listen_count'].sum()
 song_grouped['percentage'] = song_grouped['listen_count'].div(grouped_sum) * 100
-song_grouped.sort_values(['listen_count', 'title'], ascending=[0, 1])
+song_grouped.sort_values(['listen_count', 'song'], ascending=[0, 1])
 
 '''
 print(song_grouped)
 
-                                                  title  listen_count  percentage
-0     #!*@ You Tonight [Featuring R. Kelly] (Explici...            78     0.00390
-1                                                   #40           338     0.01690
-2                                                & Down           373     0.01865
-3                                          ' Cello Song           103     0.00515
-4                                    '97 Bonnie & Clyde            93     0.00465
+                                                   song  listen_count  percentage
+0                                      !!! - Sweet Life            90     0.00450
+1     'N Sync/Phil Collins - Trashin' The Camp (Phil...            94     0.00470
+2             + / - {Plus/Minus} - The Queen of Nothing           314     0.01570
+3                                     +44 - Lycanthrope            96     0.00480
+4                                  +44 - Make You Smile           130     0.00650
 ...                                                 ...           ...         ...
-9562                                      the Love Song            70     0.00350
-9563                             you were there with me            79     0.00395
-9564                   ¡Viva La Gloria! (Album Version)           171     0.00855
-9565                             ¿Lo Ves? [Piano Y Voz]            71     0.00355
-9566                                              Época           182     0.00910
+9948                       the bird and the bee - Witch           298     0.01490
+9949                the bird and the bee - You're A Cad           317     0.01585
+9950                     themselves - Skinning the Drum           108     0.00540
+9951                        tobyMac - City On Our Knees            88     0.00440
+9952  tobyMac - Lose My Soul feat. Kirk Franklin & M...            79     0.00395
 
-[9567 rows x 3 columns]
+
+[9953 rows x 3 columns]
 '''
-
 
 # We count the number of unique users and songs in our subset of data
 users = song_df['user_id'].unique()
 print(len(users))  # return 76353 unique users
-songs = song_df['title'].unique()
-print(len(songs))  # return 9567 unique songs
+songs = song_df['song'].unique()
+print(len(songs))  # return 9953 unique songs
 
 # We then create a song recommender by splitting our dataset into training and testing data.
 train_data, test_data = train_test_split(song_df, test_size=0.20, random_state=0)
