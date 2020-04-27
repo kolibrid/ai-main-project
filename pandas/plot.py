c08cba7823eb5259a460f6c78c0c2f5b18f8f1c5
khoste <44264663+khoste@users.noreply.github.com>
2020-04-27 06:14:12 +0200

    Add files via upload

---
+++ pandas/plot.py
@@ -0,0 +1,68 @@
+import pylab as pl
+import Evaluation
+import Recommenders
+import time
+import pandas
+from sklearn.model_selection import train_test_split
+
+#Read userid-songid-listen_count triplets
+#This step might take time to download data from external sources
+triplets_file = '10000.txt'
+songs_metadata_file = 'song_data.csv'
+
+song_df_1 = pandas.read_table(triplets_file,header=None)
+song_df_1.columns = ['user_id', 'song_id', 'listen_count']
+
+#Read song  metadata
+song_df_2 =  pandas.read_csv(songs_metadata_file)
+
+#Merge the two dataframes above to create input dataframe for recommender systems
+song_df = pandas.merge(song_df_1, song_df_2.drop_duplicates(['song_id']), on="song_id", how="left")
+
+song_df = song_df.head(10000)
+
+#Merge song title and artist_name columns to make a merged column
+song_df['song'] = song_df['title'].map(str) + " - " + song_df['artist_name']
+
+train_data, test_data = train_test_split(song_df, test_size = 0.20, random_state=0)
+
+pm = Recommenders.popularity_recommender_py()
+pm.create(train_data, 'user_id', 'song')
+
+is_model = Recommenders.item_similarity_recommender_py()
+is_model.create(train_data, 'user_id', 'song')
+
+
+start = time.time()
+
+#Define what percentage of users to use for precision recall calculation
+user_sample = 0.05
+
+#Instantiate the precision_recall_calculator class
+pr = Evaluation.precision_recall_calculator(test_data, train_data, pm, is_model)
+
+#Call method to calculate precision and recall values
+(pm_avg_precision_list, pm_avg_recall_list, ism_avg_precision_list, ism_avg_recall_list) = pr.calculate_measures(user_sample)
+
+end = time.time()
+print(end - start)
+
+#Method to generate precision and recall curve
+def plot_precision_recall(m1_precision_list, m1_recall_list, m1_label, m2_precision_list, m2_recall_list, m2_label):
+    pl.clf()    
+    pl.plot(m1_recall_list, m1_precision_list, label=m1_label)
+    pl.plot(m2_recall_list, m2_precision_list, label=m2_label)
+    pl.xlabel('Recall')
+    pl.ylabel('Precision')
+    pl.ylim([0.0, 0.20])
+    pl.xlim([0.0, 0.20])
+    pl.title('Precision-Recall curve')
+    #pl.legend(loc="upper right")
+    pl.legend(loc=9, bbox_to_anchor=(0.5, -0.2))
+    pl.show()
+    pl.savefig('testplot.png')
+
+print("Plotting precision recall curves.")
+
+plot_precision_recall(pm_avg_precision_list, pm_avg_recall_list, "popularity_model",
+                      ism_avg_precision_list, ism_avg_recall_list, "item_similarity_model")
